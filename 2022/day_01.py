1d394147cdd153b5942b436a9e8e4891bf4ed81b
Chiew Yan Wei <chiewyanwei@gmail.com>
2022-12-02 02:55:12 +0800

    2022 Day 1 Solution.

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -4,15 +4,18 @@ RAW = aoc_lube.fetch(year=2022, day=1)
 print(RAW)
 
 def parse_raw():
-    ...
+    return list(RAW.split("\n\n"))
 
 DATA = parse_raw()
+# print(DATA)
+
+a = sorted(map(lambda x: sum(map(int, x.split("\n"))), DATA))
 
 def part_one():
-    ...
+    return a[-1]
 
 def part_two():
-    ...
+    return sum(a[-3:])
 
 aoc_lube.submit(year=2022, day=1, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=1, part=2, solution=part_two)
