21000b0a56cabb45115032e5c6057555c87ae1cf
Chiew Yan Wei <chiewyanwei@gmail.com>
2022-12-02 17:51:53 +0800

    Add alternative solution using mod for Day 2 2022.

--- 2022/day_02.py
+++ 2022/day_02.py
@@ -30,3 +30,34 @@ def part_two():
 
 aoc_lube.submit(year=2022, day=2, part=1, solution=part_one)
 aoc_lube.submit(year=2022, day=2, part=2, solution=part_two)
+
+"""
+Some other alternative solutions I found interesting
+"""
+# Modified from Sevoi#5028 from Python Discord
+def parse_raw():
+    return [(" ABC".index(line[0]), " XYZ".index(line[2])) for line in RAW.splitlines()]
+
+DATA = parse_raw()
+print(DATA)
+
+def part_one():
+    score = 0
+    for round in DATA:
+        score += round[1]
+        if round[0] == round[1]:
+            score += 3
+        elif (round[0]) % 3 == (round[1] - 1):
+            score += 6
+    return score
+
+print(part_one())
+
+def part_two():
+    score = 0
+    for round in DATA:
+        score += 3 * (round[1] - 1)
+        score += (round[0] + round[1]) % 3 + 1
+    return score
+
+print(part_two())
