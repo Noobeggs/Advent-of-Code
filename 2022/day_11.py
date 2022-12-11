2f23553db666187f7f672111d1422ba5ba90c95e
Chiew Yan Wei <chiewyanwei@gmail.com>
2022-12-11 20:47:00 +0800

    Refactor 2022 Day 11.

--- 2022/day_11.py
+++ 2022/day_11.py
@@ -2,41 +2,6 @@ import aoc_lube
 from collections import deque
 from math import lcm
 
-RAW = aoc_lube.fetch(year=2022, day=11)
-# RAW = """Monkey 0:
-#   Starting items: 79, 98
-#   Operation: new = old * 19
-#   Test: divisible by 23
-#     If true: throw to monkey 2
-#     If false: throw to monkey 3
-
-# Monkey 1:
-#   Starting items: 54, 65, 75, 74
-#   Operation: new = old + 6
-#   Test: divisible by 19
-#     If true: throw to monkey 2
-#     If false: throw to monkey 0
-
-# Monkey 2:
-#   Starting items: 79, 60, 97
-#   Operation: new = old * old
-#   Test: divisible by 13
-#     If true: throw to monkey 1
-#     If false: throw to monkey 3
-
-# Monkey 3:
-#   Starting items: 74
-#   Operation: new = old + 3
-#   Test: divisible by 17
-#     If true: throw to monkey 0
-#     If false: throw to monkey 1"""
-print(RAW)
-
-def parse_raw():
-    return RAW.split("\n\n")
-
-DATA = parse_raw()
-
 class Monkey():
     def __init__(self, items, operator, operand, test, true, false) -> None:
         self.items = deque(items)
@@ -96,7 +61,39 @@ class Monkey():
         if self.operator == "*":
             new = self.multiply(self.items.popleft())
         return new
-def part_one():
+
+RAW = aoc_lube.fetch(year=2022, day=11)
+# RAW = """Monkey 0:
+#   Starting items: 79, 98
+#   Operation: new = old * 19
+#   Test: divisible by 23
+#     If true: throw to monkey 2
+#     If false: throw to monkey 3
+
+# Monkey 1:
+#   Starting items: 54, 65, 75, 74
+#   Operation: new = old + 6
+#   Test: divisible by 19
+#     If true: throw to monkey 2
+#     If false: throw to monkey 0
+
+# Monkey 2:
+#   Starting items: 79, 60, 97
+#   Operation: new = old * old
+#   Test: divisible by 13
+#     If true: throw to monkey 1
+#     If false: throw to monkey 3
+
+# Monkey 3:
+#   Starting items: 74
+#   Operation: new = old + 3
+#   Test: divisible by 17
+#     If true: throw to monkey 0
+#     If false: throw to monkey 1"""
+print(RAW)
+
+def parse_raw():
+    DATA = RAW.split("\n\n")
     apes = []
     for monkey in DATA:
         lines = monkey.splitlines()
@@ -111,7 +108,9 @@ def part_one():
         if_false = int(lines[5].split()[-1])
 
         apes.append(Monkey(items, operator, operand, test, if_true, if_false))
-    
+    return apes
+
+def part_one():
     inspect_count = [0] * len(apes)
     for n in range(20):
         # print(n)
@@ -130,21 +129,6 @@ def part_one():
     return monkey_business
 
 def part_two():
-    apes = []
-    for monkey in DATA:
-        lines = monkey.splitlines()
-        line_one = lines[1].split(": ")
-        items = map(int, line_one[1].split(", "))
-        line_two = lines[2].split(": ")
-        formula = line_two[1].split()
-        operator = formula[3]
-        operand = int(formula[4]) if formula[4].isdigit() else False
-        test = int(lines[3].split()[-1])
-        if_true = int(lines[4].split()[-1])
-        if_false = int(lines[5].split()[-1])
-
-        apes.append(Monkey(items, operator, operand, test, if_true, if_false))
-
     lowest_common_multiple = lcm(*[x.test for x in apes])
     inspect_count = [0] * len(apes)
     for n in range(10000):
@@ -164,5 +148,7 @@ def part_two():
     print(monkey_business)
     return monkey_business
 
+apes = parse_raw()
 aoc_lube.submit(year=2022, day=11, part=1, solution=part_one)
+apes = parse_raw()
 aoc_lube.submit(year=2022, day=11, part=2, solution=part_two)
